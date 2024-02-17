42610311fc2511e02ca3410d71d42712f891a1f0
WenderOk <127617256+WenderOk@users.noreply.github.com>
2024-02-18 00:23:39 +0300

    Anims&Modeks

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private CharacterController _characterController;
+    public Animator Amin;
 
     private float _fallVelocity = 0;
     private Vector3 _moveVector;
@@ -17,43 +18,36 @@ public class PlayerController : MonoBehaviour
         _characterController = GetComponent<CharacterController>();
     }
 
-    private void Update()
+    void Update()
     {
+        if (_moveVector != Vector3.zero) Amin.SetBool("IsWalk", true);
+        else Amin.SetBool("IsWalk", false);
+
         _moveVector = Vector3.zero;
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            _moveVector += transform.forward;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            _moveVector -= transform.forward;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            _moveVector += transform.right;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            _moveVector -= transform.right;
-        }
+        if (Input.GetKey(KeyCode.W)) _moveVector += transform.forward;
+        if (Input.GetKey(KeyCode.S)) _moveVector -= transform.forward;
+        if (Input.GetKey(KeyCode.D)) _moveVector += transform.right;
+        if (Input.GetKey(KeyCode.A)) _moveVector -= transform.right;
 
 
         if (Input.GetKeyDown(KeyCode.Space)&&_characterController.isGrounded)
         {
             _fallVelocity = -JumpForce;
+            Amin.SetBool("IsGrounded", false);
         }
     }
     void FixedUpdate()
     {
         _characterController.Move(_moveVector * Speed * Time.fixedDeltaTime);
 
-
         _fallVelocity += Gravity * Time.fixedDeltaTime;
         _characterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+
         if(_characterController.isGrounded)
         {
             _fallVelocity = 0;
+            Amin.SetBool("IsGrounded", true);
         }
     }
 }
