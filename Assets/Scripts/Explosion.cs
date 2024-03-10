0ec1ea3a2228384b1eb8c31f6f04e650dd8c4c99
WenderOk <127617256+WenderOk@users.noreply.github.com>
2024-03-10 20:45:28 +0300

    Grenades and explosions

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public float MaxSize = 5;
+    public float Speed = 1;
+    public float Damage = 30;
+    void Start()
+    {
+        transform.localScale = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale += Vector3.one*Time.deltaTime*Speed;
+        if (transform.localScale.x >= MaxSize) Destroy(gameObject);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        var playerHealth = other.GetComponent<PlayerHealth>();
+        if(playerHealth!=null) playerHealth.DealDamage(Damage);
+        
+        var enemyHealth = other.GetComponent<EnemyHealth>();
+        if (enemyHealth != null) enemyHealth.DealDamage(Damage);
+    }
+}
---
+++ Assets/Scripts/Grenade.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    public float Delay=3;
+    public GameObject ExplosionPrefab;
+    public GameObject ExplosionFX;
+    private void OnCollisionEnter(Collision collision)
+    {
+        Invoke("Explosion", Delay);
+    }
+    private void Explosion()
+    {
+        Destroy(gameObject);
+        Instantiate(ExplosionFX, transform.position, Quaternion.identity);
+        Instantiate(ExplosionPrefab,transform.position,Quaternion.identity);
+    }
+}
